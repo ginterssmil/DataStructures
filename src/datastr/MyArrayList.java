7d98ac41b15e3b70f6d5520795963b6c3e270a4c
s24smilgint <smiltinsginters@gmail.com>
2026-02-19 11:38:49 +0200

    Add: variables

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -2,4 +2,12 @@ package datastr;
 
 public class MyArrayList {
 
+	private char[] list;
+	private int howManyElements = 0;
+	private final int DEFAULT_SIZE = 8;
+	private int size = DEFAULT_SIZE;
+	
+	//konstruktori
+	
+	//funkcijas
 }
